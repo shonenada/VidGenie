fd9c3ca428bbe037cd89abbab32de72ab6cdac47
shonenada <shonenada@gmail.com>
2023-03-02 17:04:10 +0800

    feat: add build.rs, binding gl

---
+++ build.rs
@@ -0,0 +1,18 @@
+fn generate_gl_bindings() {
+    let dest = std::path::PathBuf::from(&std::env::var("OUT_DIR").unwrap());
+    let mut file = std::fs::File::create(dest.join("gl_bindings.rs")).unwrap();
+    gl_generator::Registry::new(
+        gl_generator::Api::Gles2,
+        (3, 0),
+        gl_generator::Profile::Core,
+        gl_generator::Fallbacks::All,
+        [],
+    )
+        .write_bindings(gl_generator::StructGenerator, &mut file)
+        .unwrap();
+}
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    generate_gl_bindings();
+}
