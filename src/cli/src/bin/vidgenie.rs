52af4cd00d302e2b7dcb018836a8a9f64e231e87
shonenada <shonenada@gmail.com>
2023-03-09 21:36:45 +0800

    feat: read json file and parse into render request

--- src/cli/src/bin/vidgenie.rs
+++ src/cli/src/bin/vidgenie.rs
@@ -1,4 +1,10 @@
+use std::fs::File;
+use std::io;
+use std::io::Read;
+
 use clap::Parser;
+
+use common::structs;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -7,9 +13,15 @@ struct Args {
     file: String,
 }
 
-fn main() {
+fn main() -> io::Result<()>{
     let cli = Args::parse();
     let file_path = cli.file;
+    let mut file = File::open(file_path.clone())?;
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+    let input: structs::RenderRequest = serde_json::from_str(&data)?;
 
-    println!("Genie with {}", file_path);
+    println!("Genie with {}; request: {:?}", file_path, input);
+
+    Ok(())
 }
